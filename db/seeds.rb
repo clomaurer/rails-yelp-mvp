9c4e031a1c3b6bac441011092979ee7cca85f258
Clothilde Maurer <clothilde.maurer@gmail.com>
2021-04-28 15:36:41 +0200

    yelp on rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+il_posto = Restaurant.create!(name: 'Il Posto', address: '75020', phone_number: '0000000000', category: 'italian')
+chotan = Restaurant.create!(name: 'Chotan', address: '75002', phone_number: '1111111111', category: 'chinese')
+la_frite = Restaurant.create!(name: 'La Frite', address: '75018', phone_number: '2222222222', category: 'belgian')
+balade = Restaurant.create!(name: 'Balade', address: '75012', phone_number: '3333333333', category: 'french')
+sushishop = Restaurant.create!(name: 'sushishop', address: '75015', phone_number: '4444444444', category: 'japanese')
